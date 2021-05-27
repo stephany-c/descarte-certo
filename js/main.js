d392aa281cb4d7a2b3950151fd21889fb9faa422
vncsmyrnk <vmayrink12@gmail.com>
2021-05-27 20:09:58 -0300

    Adicionando tratativas do mapa

--- js/main.js
+++ js/main.js
@@ -20,4 +20,24 @@ $(document).ready(function(){
             },
         ],
     });
+
+    // Check if user is logged
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    // Menu user account visibility
+    if (user) {
+        $('#menu_account--button').hide();
+        $('#menu_account--name').show();
+        $('#menu_account--name p').html(user.name);
+    } else {
+        $('#menu_account--button').show();
+        $('#menu_account--name').hide();
+    }
+
+    // Logout
+    $('#menu_account--logout').click(() => {
+        console.log('jkosjsjpk')
+        localStorage.setItem('user', null);
+        location.reload();
+    });
 });
