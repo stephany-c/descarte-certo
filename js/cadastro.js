cfcfce52017af782a9aedf3c9fb1a7080ffaef2e
vncsmyrnk <vmayrink12@gmail.com>
2021-06-21 21:50:01 -0300

    Adicionando os campos faltantes na tela de cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -3,31 +3,95 @@ $("#cadastro-form").submit(function(e) {
     // Prevent reload
     e.preventDefault();
 
-    // Field values
-    const name = $('#name').val();
-    const email = $('#email').val();
-    const password = $('#password').val();
+	const availableFields = [
+    	{
+			name: 'Nome',
+			element: 'name',
+			match: null
+		},
+    	{
+			name: 'Email',
+			element: 'email',
+			match: null
+		},
+    	{
+			name: 'Senha',
+			element: 'password',
+			match: null
+		},
+		{
+			name: 'Confirmação de senha',
+			element: 'password_confirmation',
+			match: 'password'
+		},
+		{
+			name: 'Sobrenome',
+			element: 'lastname',
+			match: null
+		},
+		{
+			name: 'Confirmação de email',
+			element: 'email_confirmation',
+			match: 'email'
+		},
+		{
+			name: 'Rua',
+			element: 'street',
+			match: null
+		},
+		{
+			name: 'Número',
+			element: 'streetnumber',
+			match: null
+		},
+		{
+			name: 'Bairro',
+			element: 'district',
+			match: null
+		},
+		{
+			name: 'Estado',
+			element: 'state',
+			match: null
+		}
+	];
 
-    if (!name || !email || !password) {
-        alert('Preencha todos os campos e tente novamente!');
-        return false;
-    }
+	const unMatchFields = [];
+
+	// Null validation
+	for (const field of availableFields) {
+		const fieldValue = $('#' + field.element).val();
+		if (!fieldValue) {
+			alert('Preencha todos os campos e tente novamente.');
+			return false;
+		}
+	}
+
+	// Match validation
+	const matchFields = availableFields.filter(field => field.match); 
+	for (const field of matchFields) {
+		const valueToMatch = $('#' + field.match).val();
+		const valueField = $('#' + field.element).val();
+		if (valueToMatch != valueField) {
+			unMatchFields.push(field);
+		}
+	}
+
+	if (Array.isArray(unMatchFields) && unMatchFields.length > 0) {
+		alert('Os seguintes campos não coincidem: ' + unMatchFields.map(field => field.name).join(', '));
+		return false;
+	}
 
     // Store informations
-    const newUser = {
-        name,
-        email,
-        password,
-    }
+	const newUser = {}
+	for (const field of availableFields) {
+		newUser[field.element] = $('#' + field.element).val();
+	}
 
     const users = JSON.parse(localStorage.getItem('users')) ? JSON.parse(localStorage.getItem('users')) : [];
     users.push(newUser);
 
     localStorage.setItem('users', JSON.stringify(users));
-
-    $('#name').val('');
-    $('#email').val('');
-    $('#password').val('');
 
     // Redirect
     document.location.href = 'login.html';
